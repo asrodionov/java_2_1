78caafd7f2d192a620782eb166620b20fc5a1450
asrodionov <arodionov27@yandex.ru>
2020-04-30 12:16:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int BankAccountBalance = 2_000_000_000;
+        int ReplenishmentAmount = 500_000_000;
+        int BalanceAfterReplenishment = BankAccountBalance + ReplenishmentAmount;
+        System.out.println(BalanceAfterReplenishment);
+    }
+}
+
